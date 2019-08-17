cc6948c7da1874ced11e2472a3a2f92a5cf60ecd
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-17 00:17:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,6 @@
 # My Code here....
 
 def map_to_negativize(source_array)
-  # source_array.map {|s| s * -1 }
   
-
-  negativize = source_array.each {|s| s * -1}
-  return negativize * -1
+  source_array.map {|s| s * -1 }
 end  
-
