ca4cf1d7362733c8218b8e0405fdd67775d131b0
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-17 01:09:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,3 +21,7 @@ def reduce_to_total(source_array, starting_point = 0)
   
   source_array.reduce(starting_point) {|sa ,  sp| sa + sp}
 end  
+
+def reduce_to_all_true(source_array)
+  source_array.reduce 
+end  
