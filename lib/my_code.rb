6eedf305651c8b7706499add1f730d6621df81b4
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-17 01:04:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,5 +18,6 @@ def map_to_square(source_array)
 end  
 
 def reduce_to_total(source_array, starting_point)
-  source_array.reduce {| |
+  
+  source_array.reduce(starting_point) {|sa ,  sp| sa + sp}
 end  
